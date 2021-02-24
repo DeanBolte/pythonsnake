1fe302718744301aa1704a9f74e2494ba49240dc
DeanBolte <dean.bolte7@gmail.com>
2021-02-25 08:50:40 +1100

    fixed class structure and code design

--- snake.py
+++ snake.py
@@ -38,17 +38,27 @@ menu.add_cascade(label='View', menu=viewmenu)
 viewmenu.add_command(label='Select Type')
 
 class GameBoard:
-    # Location Data
-    fruitLocation = (2, 5)
-    snakeBody = [[0]*cols for _ in range(rows)]
-    snakeDirection = "N"
+    def __init__(self, root = None): 
+        self.root = root 
 
-    # Starting location
-    snakeBody[int(rows/2)][int(cols/2)] = 1
-    snakeHead = (int(rows/2), int(cols/2))
+        # Location Data
+        self.fruitLocation = (2, 5)
+        self.snakeBody = [[0]*cols for _ in range(rows)]
+        self.snakeDirection = "N"
+
+        # Starting location
+        self.snakeBody[int(rows/2)][int(cols/2)] = 1
+        self.snakeHead = (int(rows/2), int(cols/2))
+
+        # Game Board
+        self.canvas = Canvas(root, bg="white", height=(300), width=(300))
+        self.canvas.pack(fill="both", expand="1")
+
+        # Begins moving the snake
+        self.movement() 
 
     def validLocation(location):
-        if(snakeBody[location[0]][location[1]] == 1):
+        if(self.snakeBody[location[0]][location[1]] == 1):
             return False
         elif(location[0] >= rows or location[1] >= cols):
             return False
@@ -64,19 +74,15 @@ class GameBoard:
         return location
 
     def move(x, y):
-        if(validLocation((snakeHead[0] + x, snakeHead[1] + y))):
-            snakeBody[snakeHead[0] + x][snakeHead[1] + y] = 1
+        if(self.validLocation((self.snakeHead[0] + x, self.snakeHead[1] + y))):
+            self.snakeBody[self.snakeHead[0] + x][self.snakeHead[1] + y] = 1
 
     def changeDirection(dir):
-        snakeDirection = dir
+        self.snakeDirection = dir
 
-    # Game Board
-    gameBoard = Canvas(root, bg="white", height=(300), width=(300))
-    gameBoard.pack(fill="both", expand="1")
-
-    def main():
+    def movement():
         #refresh fruit location
-        fruitLocation = newFruitLocation()
+        self.fruitLocation = self.newFruitLocation()
 
         #draw the board
         for row in range(rows):
@@ -85,24 +91,36 @@ class GameBoard:
                 y1 = row * squareLength
                 x2 = (col + 1) * squareLength
                 y2 = (row + 1) * squareLength
-                if(snakeBody[row][col] == 1):
-                    square = gameBoard.create_rectangle(x1, y1, x2, y2, fill=colourSnake, tags="area")
-                elif(row == fruitLocation[1] and col == fruitLocation[0]):
-                    square = gameBoard.create_rectangle(x1, y1, x2, y2, fill=colourFruit, tags="area")
+                if(self.snakeBody[row][col] == 1):
+                    square = self.canvas.create_rectangle(x1, y1, x2, y2, fill=colourSnake, tags="area")
+                elif(row == self.fruitLocation[1] and col == self.fruitLocation[0]):
+                    square = self.canvas.create_rectangle(x1, y1, x2, y2, fill=colourFruit, tags="area")
                 else:
-                    square = gameBoard.create_rectangle(x1, y1, x2, y2, fill=colourBack, tags="area")
+                    square = self.canvas.create_rectangle(x1, y1, x2, y2, fill=colourBack, tags="area")
 
         #move snake
-        if(snakeDirection == "N"):
+        if(self.snakeDirection == "N"):
             move(0, -1)
-        elif(snakeDirection == "E"):
+        elif(self.snakeDirection == "E"):
             move(1, 0)
-        elif(snakeDirection == "S"):
+        elif(self.snakeDirection == "S"):
             move(0, 1)
-        elif(snakeDirection == "W"):
+        elif(self.snakeDirection == "W"):
             move(-1, 0)
+
+        self.canvas.after(1000, movement)
     
-# Loop
-gameBoard = GameBoard()
-gameBoard.main()
-root.mainloop()
+if __name__ == "__main__": 
+    # object of class Tk, resposible for creating 
+    # a tkinter toplevel window  
+    gb = GameBoard(root)
+  
+    # This will bind arrow keys to the tkinter 
+    # toplevel which will navigate the image or drawing 
+    root.bind("<KeyPress-Left>", lambda e: gb.changeDirection("W")) 
+    root.bind("<KeyPress-Right>", lambda e: gb.changeDirection("E")) 
+    root.bind("<KeyPress-Up>", lambda e: gb.changeDirection("N")) 
+    root.bind("<KeyPress-Down>", lambda e: gb.changeDirection("S")) 
+      
+    # Infnite loop breaks only by interrupt 
+    root.mainloop() 
