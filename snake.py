5e9c34980eeb5531a107e61e43235ae84d3e2b6d
DeanBolte <dean.bolte7@gmail.com>
2021-02-25 08:24:05 +1100

    begun major alteration to add classes

--- snake.py
+++ snake.py
@@ -12,26 +12,9 @@ colourSnake = "#44ff44"
 colourBack = "#bbbbcc"
 colourFruit = "#ff4444"
 
-# Location Data
-fruitLocation = (2, 5)
-snakeBody = [[0]*cols for _ in range(rows)]
+#starting location
 snakeBody[int(rows/2)][int(cols/2)] = 1
-snakeDirection = "N"
-
-# Functions
-def validLocation(location):
-    if(snakeBody[location[0]][location[1]] == 1):
-        return False
-    return True
-
-def newFruitLocation():
-    # Create location
-    location = (randint(0, rows - 1), randint(0, cols - 1))
-    while(validLocation(location) == False):
-        location = (randint(0, rows - 1), randint(0, cols - 1))
-    
-    print(location)
-    return location
+snakeHead = (int(rows/2), int(cols/2))
 
 # Setup
 root = Tk()
@@ -58,37 +41,64 @@ viewmenu = Menu(menu)
 menu.add_cascade(label='View', menu=viewmenu)
 viewmenu.add_command(label='Select Type')
 
-# Game Engine
-gameBoard = Canvas(root, bg="white", height=(300), width=(300))
+class GameBoard:
+    # Location Data
+    fruitLocation = (2, 5)
+    snakeBody = [[0]*cols for _ in range(rows)]
+    snakeDirection = "N"
 
-#refresh fruit location
-fruitLocation = newFruitLocation()
+    def validLocation(location):
+        if(snakeBody[location[0]][location[1]] == 1):
+            return False
+        elif(location[0] >= rows or location[1] >= cols):
+            return False
+        return True
 
-#draw the board
-for row in range(rows):
-    for col in range(cols):
-        x1 = col * squareLength
-        y1 = row * squareLength
-        x2 = (col + 1) * squareLength
-        y2 = (row + 1) * squareLength
-        if(snakeBody[row][col] == 1):
-            square = gameBoard.create_rectangle(x1, y1, x2, y2, fill=colourSnake, tags="area")
-        elif(row == fruitLocation[1] and col == fruitLocation[0]):
-            square = gameBoard.create_rectangle(x1, y1, x2, y2, fill=colourFruit, tags="area")
-        else:
-            square = gameBoard.create_rectangle(x1, y1, x2, y2, fill=colourBack, tags="area")
+    def newFruitLocation():
+        # Create location
+        location = (randint(0, rows - 1), randint(0, cols - 1))
+        while(validLocation(location) == False):
+            location = (randint(0, rows - 1), randint(0, cols - 1))
+        
+        print(location)
+        return location
 
-#move snake
-if(snakeDirection == "N"):
+    def move(x, y):
+        if(validLocation((snakeHead[0] + x, snakeHead[1] + y))):
+            snakeBody[snakeHead[0] + x][snakeHead[1] + y] = 1
 
-elif(snakeDirection == "E"):
+    # Game Board
+    gameBoard = Canvas(root, bg="white", height=(300), width=(300))
+    gameBoard.pack(fill="both", expand="1")
 
-elif(snakeDirection == "S"):
+    def main():
+        #refresh fruit location
+        fruitLocation = newFruitLocation()
 
-elif(snakeDirection == "W"):
+        #draw the board
+        for row in range(rows):
+            for col in range(cols):
+                x1 = col * squareLength
+                y1 = row * squareLength
+                x2 = (col + 1) * squareLength
+                y2 = (row + 1) * squareLength
+                if(snakeBody[row][col] == 1):
+                    square = gameBoard.create_rectangle(x1, y1, x2, y2, fill=colourSnake, tags="area")
+                elif(row == fruitLocation[1] and col == fruitLocation[0]):
+                    square = gameBoard.create_rectangle(x1, y1, x2, y2, fill=colourFruit, tags="area")
+                else:
+                    square = gameBoard.create_rectangle(x1, y1, x2, y2, fill=colourBack, tags="area")
 
-# Pack GameBoard
-gameBoard.pack(fill="both", expand="1")
-
+        #move snake
+        if(snakeDirection == "N"):
+            move(0, -1)
+        elif(snakeDirection == "E"):
+            move(1, 0)
+        elif(snakeDirection == "S"):
+            move(0, 1)
+        elif(snakeDirection == "W"):
+            move(-1, 0)
+    
 # Loop
+main()
 root.mainloop()
